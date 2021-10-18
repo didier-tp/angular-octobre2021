9ab07a283d462456f5fff4f29aa5374c09eadb66
didierDefrance <didier@d-defrance.fr>
2021-10-18 13:51:17 +0200

    s

---
+++ tp-js/tp-ts/dist/f1.js
@@ -0,0 +1,10 @@
+"use strict";
+var a;
+var b = 5;
+b = b + 1;
+console.log("b=" + b);
+a = 8;
+a = a + 2;
+a = 2 * a;
+console.log(a);
+//# sourceMappingURL=f1.js.map
---
+++ tp-js/tp-ts/src/f1.ts
@@ -0,0 +1,8 @@
+let a :number ;
+let b =5;
+b = b+ 1;
+console.log("b="+b);
+a=8 ;
+a=a+2;
+a=2*a;
+console.log(a) ;
