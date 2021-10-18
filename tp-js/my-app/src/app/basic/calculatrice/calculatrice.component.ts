89ef9777c102f2133dc49a04d260b0fda5205fff
didierDefrance <didier@d-defrance.fr>
2021-10-18 19:32:46 +0200

    s

--- tp-js/my-app/src/app/basic/calculatrice/calculatrice.component.ts
+++ tp-js/my-app/src/app/basic/calculatrice/calculatrice.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatriceComponent implements OnInit {
 
+  a : number = 0 ;  //à saisir
+  b : number = 0 ;  //à saisir
+  res  : number = 0; //à afficher 
+
+  calculer() : void {
+     //this.res = this.a + this.b;
+     this.res = Number(this.a) + Number(this.b);
+  }
+
   constructor() { }
 
   ngOnInit(): void {
